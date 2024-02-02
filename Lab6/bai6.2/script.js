02fa566ba434caa810fa97c71d4c23216d6c03e1
npminhh <mink2kar4@gmail.com>
2024-02-02 14:13:56 +0700

    lab56

---
+++ Lab6/bai6.2/script.js
@@ -0,0 +1,13 @@
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
